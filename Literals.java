b63ac3f17a8783563c4b36d166f74c4d2dce4bbb
Stoic4mb12 <159464771+Stoic4mb12@users.noreply.github.com>
2024-07-04 19:18:52 +0530

    Learning literals and datatypes of Java.

---
+++ Literals.java
@@ -0,0 +1,15 @@
+public class Literals {
+        public static void main(String[] args) {
+            byte a = 23;
+            int b = 233;
+            long c = 23444555553l;
+            float d = 233f;
+            double e = 233232.23345;
+            char f = 'M';
+            boolean g = true;
+            String str = "Meet";
+
+
+        }
+    }
+
